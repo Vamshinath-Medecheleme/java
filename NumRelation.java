3227699626a4570ef92daef7ba615bfc44ce3b87
Vamshinath Medecheleme <Vamshi.Nath333@gmail.com>
2024-08-12 21:56:52 +0530

    Add files via upload

---
+++ NumRelation.java
@@ -0,0 +1,13 @@
+
+public class NumRelation {
+
+    public static void main(String[] args) {
+
+        int firstnum = 8;
+        int secondnum = 3;
+
+        System.out.println(firstnum==secondnum);
+        
+    }
+    
+}
