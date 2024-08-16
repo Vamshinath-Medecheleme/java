a7d10c0bfb15351c2fd44c74c3f0efe66fa9e0c3
Vamshinath Medecheleme <Vamshi.Nath333@gmail.com>
2024-08-16 18:47:28 +0530

    Add files via upload

---
+++ UnnaryOperator.java
@@ -0,0 +1,16 @@
+public class UnnaryOperator {
+
+    public static void main(String[] args) {
+        
+       int a=10;
+       int b=20;
+
+       int d = (--a)+b;
+       
+       System.out.println("hello");
+
+       System.out.println("post "+d);
+
+    }
+    
+}
