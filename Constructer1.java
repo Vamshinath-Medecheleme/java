711d9c3510fa66892b3af1adbbebb9b856d61359
Vamshinath Medecheleme <Vamshi.Nath333@gmail.com>
2024-08-19 09:02:36 +0530

    Add files via upload

---
+++ Constructer1.java
@@ -0,0 +1,25 @@
+public class Constructer1 {
+
+    int i;
+    char j;
+
+    Constructer1(int i,char j){
+        this.i=i;
+        this.j=j;
+    }
+    public static void main(String[] args) {
+
+
+      Constructer1 a = new Constructer1(24, 'h');
+
+       
+      
+      System.out.println(a.i);
+      System.out.println(a.j);
+
+         
+        
+        
+    }
+    
+}
