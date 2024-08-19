0486f58764f497b0e4673e2322e6d16ebddc8b12
Vamshinath Medecheleme <Vamshi.Nath333@gmail.com>
2024-08-19 09:02:15 +0530

    Add files via upload

---
+++ SimpleMethod.java
@@ -0,0 +1,16 @@
+public class SimpleMethod {
+
+    public static void main(String[] args) {
+
+        SimpleMethod m = new SimpleMethod();
+        m.sayHello();
+        
+
+
+    }
+
+    void sayHello(){
+        System.out.println("hello");
+    }
+    
+}
