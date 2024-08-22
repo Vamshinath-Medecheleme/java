f0746f23deb621c318db103042967f9c03e44004
Vamshinath Medecheleme <Vamshi.Nath333@gmail.com>
2024-08-22 12:09:11 +0530

    Add files via upload

---
+++ WhileOperator5.java
@@ -0,0 +1,12 @@
+public class WhileOperator5{
+
+    public static void main(String[] args) {
+        
+        int i =0;
+        while(i<7){
+        System.out.println(i);
+        i++;
+        }
+    }
+    
+}
