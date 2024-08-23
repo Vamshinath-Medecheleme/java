0b05344179c3001e83ddd7feb5cb9ee1dd0a646b
Vamshinath Medecheleme <Vamshi.Nath333@gmail.com>
2024-08-23 08:37:04 +0530

    Add files via upload

---
+++ TernaryOperator2.java
@@ -0,0 +1,12 @@
+public class TernaryOperator2 {
+    public static void main(String[] args) {
+        
+        int i=40;
+        int j=60;
+
+        int max= (i>j)?i:j;
+
+        System.out.println(max);
+    }
+    
+}
