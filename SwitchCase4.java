c2f23a8a3030250b97a0c70cbf9a3f05eda44f01
Vamshinath Medecheleme <Vamshi.Nath333@gmail.com>
2024-08-24 09:51:35 +0530

    Add files via upload

---
+++ SwitchCase4.java
@@ -0,0 +1,26 @@
+public class SwitchCase4 {
+    public static void main(String[] args) {
+
+        int num=2;
+        String day;
+
+        switch (num) {
+            case 1:
+            day="sunday";
+                break;
+
+                case 2:
+                day="monday";
+                break;
+
+                case 3:
+                day="tuesday";
+        
+            default:
+            day="raviteja";
+                break;
+        }
+        System.out.println(day);
+    }
+    
+}
