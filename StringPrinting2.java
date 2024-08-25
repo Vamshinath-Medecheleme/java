65da954f47133704d2b44343342f4f006dd29a97
Vamshinath Medecheleme <Vamshi.Nath333@gmail.com>
2024-08-25 22:28:18 +0530

    Add files via upload

---
+++ StringPrinting2.java
@@ -0,0 +1,8 @@
+public class StringPrinting2 {
+    public static void main(String[] args) {
+        StringBuilder sc = new StringBuilder("hello raman");
+
+        System.out.println(sc);
+    }
+    
+}
