74d1fccddc9ba84cd9127c518276e3ae9cfef2e4
Vamshinath Medecheleme <Vamshi.Nath333@gmail.com>
2024-08-26 17:06:53 +0530

    Add files via upload

---
+++ SimpleMethod2.java
@@ -0,0 +1,17 @@
+public class SimpleMethod2 {
+    public static void main(String[] args) {
+
+        SimpleMethod2 m = new SimpleMethod2();
+        int x=m.addition(10, 20);
+        System.out.println(x);
+    }
+
+    int addition(int a, int b){
+
+        int x = a+b;
+
+        return x;
+    }
+
+    
+}
