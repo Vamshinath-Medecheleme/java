f6792c50e47adc22d25145e38b8f27ce2e519405
Vamshinath Medecheleme <Vamshi.Nath333@gmail.com>
2024-09-08 12:34:02 +0530

    Add files via upload

---
+++ EvenNumberPrinter.java
@@ -0,0 +1,18 @@
+class EvenNumberPrinter {
+    public static void main(String[] args) {
+       
+       int number=1;
+       while(number<=20){
+           
+          if(number%2==0){
+               System.out.println(number);
+           
+          }
+          number++;
+        
+       }
+      
+
+
+    }
+}
