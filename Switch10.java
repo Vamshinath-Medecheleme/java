1c9c507b7928464f8afc9e2d980a7a4397ec837c
Vamshinath Medecheleme <Vamshi.Nath333@gmail.com>
2024-09-10 22:27:35 +0530

    Add files via upload

---
+++ Switch10.java
@@ -0,0 +1,21 @@
+public class Switch10 {
+    public static void main(String[] args) {
+        
+        int num=3;
+        String day;
+
+        switch(num){
+
+            case 1:day="sunday";
+            break;
+            case 2:day="monday";
+            break;
+            case 3:day="tuesday";
+            break;
+            default:day="sunnaday";
+        
+        }
+        System.out.println(day);
+    }
+    
+}
