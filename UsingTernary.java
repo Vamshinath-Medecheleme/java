5267ded31761e1918e34b438d80d5837fff76091
Vamshinath Medecheleme <Vamshi.Nath333@gmail.com>
2024-09-12 18:05:29 +0530

    Add files via upload

---
+++ UsingTernary.java
@@ -0,0 +1,10 @@
+public class UsingTernary {
+    public static void main(String[] args) {
+
+
+        int marks = 45;
+        
+        String test =marks>26?"pass":"fail";
+        System.out.println(test);
+    }
+}
