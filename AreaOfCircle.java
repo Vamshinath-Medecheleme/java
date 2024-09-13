a248b9890d98bfd3ec070aa43a18b5445c954b51
Vamshinath Medecheleme <Vamshi.Nath333@gmail.com>
2024-09-13 16:07:38 +0530

    Add files via upload

---
+++ AreaOfCircle.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class AreaOfCircle {
+    public static void main(String[] args) {
+        
+        //Area of circle = piR^2
+
+        final float pi = 3.14f;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("radius : ");
+         int radius = scanner.nextInt();
+
+         float circleArea = pi*(radius*radius);
+
+         System.out.println(circleArea);
+
+         scanner.close();
+
+
+
+    }
+    
+}
