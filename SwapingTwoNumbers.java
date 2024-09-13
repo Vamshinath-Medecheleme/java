d039a28d15a43dce47035d34e5230828b4d74ddf
Vamshinath Medecheleme <Vamshi.Nath333@gmail.com>
2024-09-13 16:20:57 +0530

    Add files via upload

---
+++ SwapingTwoNumbers.java
@@ -0,0 +1,18 @@
+public class SwapingTwoNumbers {
+    public static void main(String[] args) {
+        
+        int i = 5;
+        int j = 10;
+
+        int temp = i;
+
+        i=j;
+
+        j=temp;
+       
+        System.out.println(i);
+        System.out.println(j);
+
+    }
+    
+}
