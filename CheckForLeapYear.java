a479a07a7fb1ac627a5070ea0e690afce401c592
Vamshinath Medecheleme <Vamshi.Nath333@gmail.com>
2024-09-15 09:23:04 +0530

    Add files via upload

---
+++ CheckForLeapYear.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class CheckForLeapYear {
+    public static void main(String[] args) {
+        
+        /*A leap year is exactly divisible by 4 except for century years (years ending with 00).
+         The century year is a leap year only if it is perfectly divisible by 400. */
+
+         Scanner scanner = new Scanner(System.in);
+         System.out.print("Enter year : ");
+         int year = scanner.nextInt();
+
+         if(year%4==0 && year*100%4!=0 || year*100%400==0){
+
+            System.out.println("it is a year");
+         }
+         else{
+            System.out.println("Not a leap yr");
+         }
+
+
+         scanner.close();
+
+
+    }
+    
+}
