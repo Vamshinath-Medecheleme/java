33725fcd0726bb9c2e329b777db6f80bbb133ae7
Vamshinath Medecheleme <Vamshi.Nath333@gmail.com>
2024-09-16 12:04:25 +0530

    Add files via upload

---
+++ Factorial.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = scanner.nextInt();
+        
+        int result=1;
+
+        for(int i=1; i<=n; i++){
+            result*=i;
+        }
+        System.out.println("Factorial of : "+n+" is "+result);
+
+        scanner.close();
+    }
+}
+
+
+
