ccec9d12e70b68cfef79a8127faaa24fa927e3cf
Vamshinath Medecheleme <Vamshi.Nath333@gmail.com>
2024-09-19 23:24:12 +0530

    Add files via upload

---
+++ StringToUppercase.java
@@ -0,0 +1,7 @@
+public class StringToUppercase {
+    public static void main(String[] args) {
+        String str = "hello world";
+        String uppercase = str.toUpperCase();
+        System.out.println(uppercase); // Output: HELLO WORLD
+    }
+}
