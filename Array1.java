c29a12df62f1a5fbe77e12ce428f352f86e68903
Vamshinath Medecheleme <Vamshi.Nath333@gmail.com>
2024-09-21 23:35:11 +0530

    Add files via upload

---
+++ Array1.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+public class Array1 {
+    public static void main(String[] args) {
+        
+        int arr[] = {1,2,3};
+
+        
+        System.out.println(Arrays.toString(arr));
+       
+        
+    }
+    
+}
