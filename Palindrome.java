06ca01e8d46d7ac3678f3d1d13b1a664d9d6ead4
Vamshinath Medecheleme <Vamshi.Nath333@gmail.com>
2024-10-02 20:07:08 +0530

    Add files via upload

--- Palindrome.java
+++ Palindrome.java
@@ -3,8 +3,6 @@ import java.util.Scanner;
 public class Palindrome {
     public static void main(String[] args) {
 
-        
-        
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("str : ");
