b9d498d7dd676281ac40c7dea06a5dbbb57f46a8
Vamshinath Medecheleme <Vamshi.Nath333@gmail.com>
2024-10-06 23:49:49 +0530

    Add files via upload

---
+++ NumInRange.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class NumInRange{
+    public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in);
+       System.out.println("type num : ");
+       int num = sc.nextInt();
+       
+       if(num>=1&&num<=100){
+           System.out.println("it is in range");
+       }
+       else{
+           System.out.println("it is not in range");
+       }
+       sc.close();
+    }
+}
